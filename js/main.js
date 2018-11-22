ab2a38907dc6bc6cc87bf645bd31fd12c4669e5d
giordifungula <giordifungula@gmail.com>
2018-11-22 08:52:15 -0800

    styling responsive version and working on js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// // 
+// var menuBar = document.getElementById('menu-bar');
+// menuBar.addEventListener('click', function(){
+//     var menu = document.getElementById('mobile-nav');
+//     console.log(1);
+    
+    
+// } );
+// function toggleMenu(){
+//     // menu.className += 'display';
+// }
