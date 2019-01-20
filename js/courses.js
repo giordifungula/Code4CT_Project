a504a7e17afccb862ffcafd7049a96e2f0a9a229
giordifungula <giordifungula@gmail.com>
2019-01-20 23:29:09 +0200

    courses able to delete and add update

---
+++ js/courses.js
@@ -0,0 +1,270 @@
+'use strict';
+
+
+
+let totalAmount;
+let cartNumber = 0;
+
+// Store Cart Heading and CardContent
+let cartHeading = document.getElementById('cartHeading');//$('#cartHeading');
+let cartContent = document.getElementById('cart');
+
+// Show and Hide the Cart
+cartHeading.addEventListener('click', function(){
+    cartContent.classList.toggle('displayCart');
+    // console.log(cartContent);
+});
+
+// Store cart Buttons
+let cartbtn = document.querySelectorAll('.cartItem');
+
+
+cartbtn.forEach(function(btn){
+    // when clicked on any button
+    // let cartItemtext = btn.childNodes[0].textContent;
+    //    let name = btn.target.parentElement.children[0].textContent;
+    //     let price = btn.target.parentElement.children[3].textContent;
+    btn.addEventListener('click', function(e){
+        alert('item added');
+        // Empty Object
+        let item = {
+
+        };
+        // Grabs the Name of Course
+        let name = e.target.parentElement.children[0].textContent;
+        item.name = name;
+        // Grabs the price of course
+        let price = e.target.parentElement.children[3].textContent;
+        item.price = price;
+        // Extracts the string characters
+
+        let finalPrice = price.slice(6).trim();
+        item.price = finalPrice;
+
+        // Create Cart Item
+
+        let cartItem = document.createElement('div');
+        // adds the cartItem class to created div
+        cartItem.classList.add('cartItem');
+        // adds template HTML content
+
+        cartItem.innerHTML = 
+        `
+                       <div class="cartItemText">
+                        <p id="cartItemName" class="">${item.name}</p>
+                        <span>R</span>
+                        <span id="cartItemPrice" class="cartItemPrice">${item.price}</span>
+                    </div>
+                    <div class="updateBtn">
+                        <a href="#" id="cartItemRemove" class="cartItemRemove">
+                                <i class="fas fa-trash">Remove</i>   
+                        </a>
+                        <a href="" id="cartItemUpdate" class="cartItemUpdate">
+                            <i class="fas fa-plus"></i>
+                        </a>
+                    </div>
+        `
+        let updatedCart = document.getElementById('cart');
+        let total = document.getElementById('cartTotalPrice');
+        cartContent.appendChild(cartItem, total);
+        displayTotals();
+        let deleteBtn = document.querySelectorAll('.cartItemRemove');
+        deleteBtn.forEach(function(btn){
+            btn.addEventListener('click',function(e){
+                let removeItem = e.target.parentElement.parentElement.parentElement.remove();
+                alert('Your Cart is cleared','Please add another Item !');
+                // document.getElementById('cartTotalPrice').innerHTML = 0;
+
+                // let previousAmount = document.getElementById('cartTotalPrice').innerText.remove();
+                let itemRow = e.target.parentElement.parentElement.parentElement;
+                let itemRowPrices = itemRow.getElementsByClassName('cartItemPrice')[0];
+                // Capture the Prices Container
+
+                for(let i =0;i<itemRowPrices.length;i++){
+                    let updatePrice = document.getElementById('cartTotalPrice');
+                    // updatePrice.textContent = '';
+                    // Loop through all the Newly added content 
+                    let total = 0;
+                    let itemRowPrice = itemRowPrices[i];
+                    let priceTag = itemRowPrice.getElementsByClassName('cartItemPrice')[0].textContent[0];
+                    let priceNumber=parseInt(priceTag.innerHTML);
+                    if(itemRowPrice + itemRowPrice === updatePrice){
+                        console.log('Thery are the same')
+                    } else {
+                        updatePrice.textContent = itemRowPrice;
+                    }
+                    // console.log(priceTag.innerHTML, 'first');
+                    // console.log(updatePrice.innerHTML);
+                    // let price = parseInt(priceTag.innerText = i);
+
+                }
+                console.log(itemRow);
+                console.log(itemRowPrices);
+
+            });
+        });
+        console.log(deleteBtn);
+
+
+        console.log(cartItem);
+
+        console.log(finalPrice);
+        // let numberText = parseInt(price);
+        // price.splice(1);
+        
+        console.log(name);
+        console.log(price);
+        // addCart(name, price);
+        
+    });
+        // alert('Item added')
+        // Will show how many items are inside the array
+        function displayTotals(){
+            console.log('total');
+            let totalPrices = [];
+            let items = document.querySelectorAll('.cartItemPrice');
+            // items.pop();
+            console.log(items);
+            items.forEach(function(item){
+                // Makes sure its a Number and not string
+                if(item === 0){
+                    item.remove();
+                }
+                totalPrices.push(parseInt(item.textContent));
+            });
+            console.log(totalPrices);
+            let finalMoney = totalPrices.reduce(function(totalPrices, item){
+                totalPrices += item;
+                return totalPrices;
+            });
+            console.log(finalMoney);
+            let total = document.getElementById('cartTotalPrice');
+
+       total.textContent = finalMoney;
+       document.getElementById('cartItemPrice').textContent = totalPrices.length -1;
+    
+    }
+        // l
+
+
+});
+
+
+console.log(cartHeading);
+// remove Item 
+
+// let cartItem = document.getElementsByClassName('cartItem');
+
+// Courses Items stored
+var course1 = document.getElementById('course1');
+// Buttons stored
+// cartbtn.forEach(function(btn){
+//     // when clicked on any button
+//     btn.addEventListener('click', function(e){
+//         alert('Item added');
+//     let cartItemtext = btn.childNodes[0].textContent;
+//        let name = e.target.parentElement.children[0].textContent;
+//         let price = e.target.parentElement.children[3].textContent;
+//         console.log(name);
+//         console.log(price);
+//         // console.log(cartItemtext);
+//     });
+// });
+
+
+
+
+
+// btn.addEventListener('click', addCart(name, price))
+// {
+    // alert('Item added');
+//    let cartItemtext = btn.childNodes[0].textContent;
+//     name = e.target.parentElement.children[0].textContent;
+//     price = e.target.parentElement.children[1].textContent;
+//     console.log()
+//     // console.log(cartItemtext);
+// });
+// });
+// console.log(cartbtn);
+
+// course1.addEventListener('click', addCart('Course1', 2000));
+course1.addEventListener('click', function(){
+    // alert('course1 added');
+});
+let cart = [
+//     { name: name,
+//     price: price,
+// },
+    // { name: name, price: price,},
+    // { name: name, price: price,},
+    // { name: name, price: price,},
+    // { name: name, price: price,},
+    // { name: name, price: price,},
+];
+
+// function addCart(name, price) {
+//     //create item
+//     let item = {
+//         name: name,
+//         price: price
+//     };
+    
+//     if(cart.length === 0 || cart.length >= 1){
+//         // let check = false;
+//         // for(let cartItem in cart){
+//         //     if(cart[cartItem].name === item.name){
+//         //         check = true;
+//         //         }
+//         // }
+//         cart.push(item);
+//     }
+//     //check if array empty
+//     // if (cart.length != 0) {
+//     //     //search an item in an array
+//     //     let check = false;
+//     //     for (let cartItem in cart) {
+//     //         if (cart[cartItem].name === item.name) {
+//     //             check = true;
+//     //         }
+//     //     }
+
+//     //     //if check returns false,  item was not found in the array.
+//     //     if (!check) { cart.push(item) }
+
+//     //     //else push to array
+//     // } else {
+//     //     cart.push(item);
+//     // }
+//     // if(cart.length === 0){
+//     //     // let check = false;
+//     //     // for(let cartItem in cart){
+//     //     //     if(cart[cartItem].name === item.name){
+//     //     //         check = true;
+//     //     //      }
+//     //     // }
+//     //     cart.push(item); 
+//     //     console.log(cart);
+//     // }
+
+//     //get cart div
+//     let div = document.getElementById('cart');
+//     //clear cart
+//     div.innerHTML = "People";
+
+//     //show cart with updated array
+//     for (let value in cart) {
+
+//         // Create a <li> node
+//         let node = document.createElement("LI");
+//         // Create a text node               
+//         let textnode = document.createTextNode(cart[value].name);
+
+//         // Append the text to <li>
+//         node.appendChild(textnode);
+
+//         // Append <li> to <ul> with id="myList"                             
+//         div.appendChild(node);
+//     }
+// }
+
+
