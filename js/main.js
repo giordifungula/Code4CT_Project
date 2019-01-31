45202c8136313d58196253db8fc09c2154853c76
giordifungula <giordifungula@gmail.com>
2019-01-31 10:52:37 +0200

    removed the toggleNav funtion on mobile

--- js/main.js
+++ js/main.js
@@ -15,11 +15,7 @@ $(document).ready(function(){
         console.log($('#mobile-nav').toggleClass('open'));
         // $('#mobile-nav').style.height = '100px';
     });
-    // ===== CounterUp ===== ;
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
+    
 
     // ==== Slider ====
     $(".center").slick({
@@ -33,63 +29,7 @@ $(document).ready(function(){
         slidesToScroll: 4
       });
 
-    // ===== Progress Bar ======
     
-    // var progress_circle1 = $(".progress-bar1").circularProgress({
-    //         // options here
-    //         line_width: 6,
-    //         color: "#ee534f ",
-    //         starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
-    //         percent: 70, // percent starts from
-    //         percentage: true
-    //         // text: "More power behind every pixel"
-    //         }).circularProgress('animate', 70, 3000);
-    //     // });
-    //     var progress_circle2 = $(".progress-bar2").circularProgress({
-    //         // options here
-    //         line_width: 6,
-    //         color: "#ee534f ",
-    //         starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
-    //         percent: 0, // percent starts from
-    //         percentage: true
-    //         // text: "More power behind every pixel"
-    //         }).circularProgress('animate', 74, 3000);
-    //     var progress_circle3 = $(".progress-bar3").circularProgress({
-    //         // options here
-    //         line_width: 6,
-    //         color: "#ee534f ",
-    //         starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
-    //         percent: 70, // percent starts from
-    //         percentage: true
-    //         // text: "More power behind every pixel"
-    //         }).circularProgress('animate', 56, 3000);
-    //     var progress_circle4 = $(".progress-bar4").circularProgress({
-    //         // options here
-    //         line_width: 6,
-    //         color: "#ee534f ",
-    //         starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
-    //         percent: 0, // percent starts from
-    //         percentage: true
-    //         // text: "More power behind every pixel"
-    //         }).circularProgress('animate', 24, 3000);
-    //     var progress_circle5 = $(".progress-bar5").circularProgress({
-    //         // options here
-    //         line_width: 6,
-    //         color: "#ee534f ",
-    //         starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
-    //         percent: 0, // percent starts from
-    //         percentage: true
-    //         // text: "More power behind every pixel"
-    //         }).circularProgress('animate', 80, 3000);
-    //     var progress_circle6 = $(".progress-bar6").circularProgress({
-    //         // options here
-    //         line_width: 6,
-    //         color: "#ee534f ",
-    //         starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
-    //         percent: 0 // percent starts from
-    //         // percentage: false
-    //         // text: "More power behind every pixel"
-    //         }).circularProgress('animate', 10, 3000);
 
             /*Scroll to top when arrow up clicked BEGIN*/
         $(window).scroll(function() {
@@ -110,6 +50,11 @@ $(document).ready(function(){
 });
  /*Scroll to top when arrow up clicked END*/
             
+        // ===== CounterUp ===== ;
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
 });
 
 
